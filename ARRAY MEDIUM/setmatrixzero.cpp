07922dddc8793e492857e2081e14b2e7695b0782
Prakhar <praku130107@gmail.com>
2026-06-21 23:15:57 +0530

    "tweaked optimal sol a lil bit"

--- ARRAY MEDIUM/setmatrixzero.cpp
+++ ARRAY MEDIUM/setmatrixzero.cpp
@@ -154,71 +154,90 @@ int main()
 }
 
 // optimal time
-class Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+void setZeroes(vector<vector<int>> &matrix)
 {
-public:
-    void setZeroes(vector<vector<int>> &matrix)
+    int n = matrix.size();
+    int m = matrix[0].size();
+
+    int col0 = 1;
+
+    // Step 1: Mark rows and columns
+    for (int i = 0; i < n; i++)
     {
-
-        int n = matrix.size();
-        int m = matrix[0].size();
-
-        int col0 = 1;
-
-        // Step 1: Mark rows and columns
-        for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
         {
-            for (int j = 0; j < m; j++)
-            {
-                if (matrix[i][j] == 0)
-                {
-                    // mark row
-                    matrix[i][0] = 0;
-
-                    // mark column
-                    if (j != 0)
-                    {
-                        matrix[0][j] = 0;
-                    }
-                    else
-                    {
-                        col0 = 0;
-                    }
-                }
-            }
-        }
-
-        // Step 2: Set zeros except first row and first column
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = 1; j < m; j++)
-            {
-                if (matrix[i][j] != 0)
-                {
-                    if (matrix[i][0] == 0 || matrix[0][j] == 0)
-                    {
-                        matrix[i][j] = 0;
-                    }
-                }
-            }
-        }
-
-        // Step 3: Handle first row
-        if (matrix[0][0] == 0)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                matrix[0][j] = 0;
-            }
-        }
-
-        // Step 4: Handle first column
-        if (col0 == 0)
-        {
-            for (int i = 0; i < n; i++)
+            if (matrix[i][j] == 0)
             {
                 matrix[i][0] = 0;
+
+                if (j != 0)
+                {
+                    matrix[0][j] = 0;
+                }
+                else
+                {
+                    col0 = 0;
+                }
             }
         }
     }
-};
+
+    // Step 2: Fill matrix using markers
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    // Step 3: Handle first row
+    if (matrix[0][0] == 0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Step 4: Handle first column
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i][0] = 0;
+        }
+    }
+}
+
+int main()
+{
+    vector<vector<int>> matrix =
+        {
+            {1, 1, 1, 1},
+            {1, 0, 1, 1},
+            {1, 1, 0, 1},
+            {1, 1, 1, 1}};
+
+    setZeroes(matrix);
+
+    for (auto row : matrix)
+    {
+        for (auto ele : row)
+        {
+            cout << ele << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
