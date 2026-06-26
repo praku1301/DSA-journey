2e1439c603901cd64d4b29c3928f80c494ce05c8
Prakhar <praku130107@gmail.com>
2026-06-26 13:12:45 +0530

    "PASCAL TRIANGLE HARD"

--- ARRAY HARD/pascaltriangle3types.cpp
+++ ARRAY HARD/pascaltriangle3types.cpp
@@ -89,3 +89,57 @@ int main()
 
     return 0;
 }
+
+// optimL PRINTING THE WHOLE TRIANGLE
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> generateRow(int row)
+{
+    long long ans = 1;
+    vector<int> ansRow;
+
+    ansRow.push_back(1);
+
+    for (int col = 1; col < row; col++)
+    {
+        ans = ans * (row - col);
+        ans = ans / col;
+
+        ansRow.push_back(ans);
+    }
+
+    return ansRow;
+}
+
+vector<vector<int>> pascalTriangle(int N)
+{
+    vector<vector<int>> ans;
+
+    for (int row = 1; row <= N; row++)
+    {
+        ans.push_back(generateRow(row));
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    vector<vector<int>> ans = pascalTriangle(N);
+
+    for (auto row : ans)
+    {
+        for (auto val : row)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
